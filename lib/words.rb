c0c816db6617e931ec8b8337fb0c3b7289a6733e
ngottlieb87 <nogpdx@gmail.com>
2017-09-15 09:22:24 -0700

    add initial spec for adding words to a list

--- lib/words.rb
+++ lib/words.rb
@@ -1,7 +1,33 @@
 class Word
-  def inintialize()
+  @@word_list = []
+
+  attr_reader(:id)
+  attr_accessor(:input_word, :definition)
+
+  def initialize(attributes)
+    @word = attributes.fetch(:input_word)
+    @def = attributes.fetch(:definition)
+    @id = @@word_list.length + 1
   end
 
-  def method()
+  def self.all()
+    @@word_list
+  end
+
+  def self.find(id)
+    word_id = id.to_i
+    @@word_list.each do |word|
+      if words.id == word_id
+        return word
+      end
+    end
+  end
+
+  def self.clear()
+    @@word_list = []
+  end
+
+  def save()
+    @@word_list.push(self)
   end
 end
--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -2,8 +2,31 @@ require "rspec"
 require "pry"
 require "words"
 
-describe("Word") do
-  before() do
-    Word.clear()
+  describe("Word") do
+    before() do
+      Word.clear()
+    end
+
+  describe(".all") do
+    it("list begins with no content") do
+    expect(Word.all()).to(eq([]))
+  end
+end
+
+  describe("#save") do
+    it("it will save a word to a list of words") do
+      word = Word.new({:input_word=>"Zebra", :definition=>"A wild african horse"})
+      word.save()
+      expect(Word.all()).to(eq([word]))
+    end
+  end
+
+  describe(".clear") do
+    it("clear previous word from list to not recieve duplicates to list") do
+      word= Word.new({:input_word=> "Rabbit", :definition=> "a burrowing, gregarious, plant-eating mammal with long ears, long hind legs, and a short tail."})
+      word.save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
+    end
   end
 end
